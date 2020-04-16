017b7df8867cbba482b6b2fe58b752963c6c5ba8
iProxys <edwinavahh@outlook.com>
2020-04-15 21:39:05 -0600

    Cambios en el inicio.html

--- assets/components/php/inicio.php
+++
@@ -1,2 +0,0 @@
-<h1>Contenido de inicio</h1>
-
---
+++ assets/components/php/panel.php
@@ -0,0 +1,119 @@
+<div class="row">
+    <div class="col-12 col-lg-4">
+        <div class="m-3">
+            <div class="row bg-info rounded-top card-top text-white">
+                <div class="col-2 p-4">
+                    <i class="fas fa-server icon"></i>
+                </div>
+                <div class="col-10 text-right p-4">
+                    <h1>6</h1>
+                    <small>Todos los elementos</small>
+                </div>
+            </div>
+
+            <div class="row rounded-bottom card-bottom border border-info text-info">
+                <div class="col-10 mt-3 pl-4">
+                    <p>Ver detalles</p>
+                </div>
+                <div class="col-2 mt-3 text-right pr-4">
+                    <i class="far fa-arrow-alt-circle-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-12 col-lg-4">
+        <div class="m-3">
+            <div class="row bg-warning rounded-top card-top text-white">
+                <div class="col-2 p-4">
+                    <i class="fas fa-exclamation-triangle icon"></i>
+                </div>
+                <div class="col-10 text-right p-4">
+                    <h1>6</h1>
+                    <small>Elementos a punto de agotar</small>
+                </div>
+            </div>
+
+            <div class="row rounded-bottom card-bottom border border-warning text-warning">
+                <div class="col-10 mt-3 pl-4">
+                    <p>Ver detalles</p>
+                </div>
+                <div class="col-2 mt-3 text-right pr-4">
+                    <i class="far fa-arrow-alt-circle-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-12 col-lg-4">
+        <div class="m-3">
+            <div class="row bg-danger rounded-top card-top text-white">
+                <div class="col-2 p-4">
+                    <i class="fas fa-exclamation-circle icon"></i>
+                </div>
+                <div class="col-10 text-right p-4">
+                    <h1>6</h1>
+                    <small>Elementos eliminados recientemente</small>
+                </div>
+            </div>
+
+            <div class="row rounded-bottom card-bottom border border-danger text-danger">
+                <div class="col-10 mt-3 pl-4">
+                    <p>Ver detalles</p>
+                </div>
+                <div class="col-2 mt-3 text-right pr-4">
+                    <i class="far fa-arrow-alt-circle-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-12 col-lg-4">
+        <div class="m-3">
+            <div class="row bg-success rounded-top card-top text-white">
+                <div class="col-2 p-4">
+                    <i class="fas fa-users icon"></i>
+                </div>
+                <div class="col-10 text-right p-4">
+                    <h1>6</h1>
+                    <small>Usuarios registrados</small>
+                </div>
+            </div>
+
+            <div class="row rounded-bottom card-bottom border border-success text-success">
+                <div class="col-10 mt-3 pl-4">
+                    <p>Ver detalles</p>
+                </div>
+                <div class="col-2 mt-3 text-right pr-4">
+                    <i class="far fa-arrow-alt-circle-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-12 col-lg-4">
+        <div class="m-3">
+            <div class="row bg-dark rounded-top card-top text-white">
+                <div class="col-2 p-4">
+                <i class="far fa-edit icon"></i>
+                </div>
+                <div class="col-10 text-right p-4">
+                    <h1>6</h1>
+                    <?php
+                        ini_set('date.timezone', 'America/Mexico_City');
+                    ?>
+                    <small>Modificaciones hoy, <?php echo date("d-m-Y"); ?></small>
+                </div>
+            </div>
+
+            <div class="row rounded-bottom card-bottom border border-dark text-dark">
+                <div class="col-10 mt-3 pl-4">
+                    <p>Ver detalles</p>
+                </div>
+                <div class="col-2 mt-3 text-right pr-4">
+                    <i class="far fa-arrow-alt-circle-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
