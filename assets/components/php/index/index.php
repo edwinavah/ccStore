af8844a237d5f4f5ae5eba4e1aea5d5136f5160c
Nathalia Calzadilla <nathaliac8a@gmail.com>
2020-04-24 19:00:13 -0600

    Required

--- assets/components/php/index/index.php
+++ assets/components/php/index/index.php
@@ -7,19 +7,19 @@
                 <img src="assets/images/logo.svg" />
             </div>
             <!-- FORMULARIO -->
-            <form class="col-12" id="entrar" method="POST">
+            <form class="col-12" id="entrar" action="assets/components/php/index/iniciarSesion.php" method="POST">
                 <div class="user-icono">
-                    <input type="text" placeholder="Nombre de Usuario" id="usuario" name="usuario" require>
+                    <input type="text" placeholder="Nombre de Usuario" id="usuario" name="usuario" required>
                 </div>
                 <div class="password-icono">
-                    <input type="password" placeholder="Contraseña" id="contrasena" name="contrasena" require>
+                    <input type="password" placeholder="Contraseña" id="contrasena" name="contrasena" required>
                 </div>
 
                             <div id="respuesta3" style="text-align: center;"></div>
 
                 <div class="row mb-4">
                     <div class="col-12">
-                        <button type="button" class="btn btn-primary" id="buttonlg"><i class="fas fa-sign-in-alt"></i> Entrar</button>
+                        <button type="sumbit" class="btn btn-primary" id="buttonlg"><i class="fas fa-sign-in-alt"></i> Entrar</button>
                     </div>
                 </div>
             </form>
@@ -27,8 +27,7 @@
     </div>
 </div>
 <script>
-    
-    $('#buttonlg').on('click',function(){
+    $(function() {
         $("#entrar").validate({
         rules: {
             usuario: {
@@ -43,17 +42,5 @@
                 contrasena: "Please provide some data"
                 }
         });
-        $.ajax({
-            url: 'assets/components/php/index/iniciarSesion.php',
-            type: 'POST',
-            data: $('#entrar').serialize(),
-            success: function(res){
-                $('#respuesta3').html(res);
-            }
-        }).done(function(data){
-            //limpia los campos despues de hacer la peticion 
-            $('#entrar').trigger("reset");
-        });
     });
-
     </script>
