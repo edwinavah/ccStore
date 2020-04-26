613cbc77bed4d6bdb2b01065d9479c4036b6183a
iProxys <edwinavahh@outlook.com>
2020-04-26 15:21:04 -0600

    Redireccionando a pagina indes al no iniciar sesion

--- salidaProductos.php
+++ salidaProductos.php
@@ -32,13 +32,7 @@
             }
             else {
                 //NO INICIO SESION
-                echo 
-                '<div class="text-center mt-5">
-                    <img src="assets/images/ccStore_Azul.svg" alt="" style="width: 400px; height: auto;">
-                    <h1 class="mt-2"><strong>Upps!</strong></h1>
-                    <p>Necesitas iniciar sesión para acceder a esta pagina. <a href="index.php">Haga clic aquí para iniciar sesión</a></p>
-                </div>';
-                exit;
+                header("Location: index.php");
             }
             // SESION FINALIZADA POR INACTIVIDAD
             $now = time();          
