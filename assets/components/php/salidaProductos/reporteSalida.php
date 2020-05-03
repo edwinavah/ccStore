9e16c398a752d71500891c79f84cb98420fa8802
iProxys <edwinavahh@outlook.com>
2020-05-03 17:29:28 -0600

    Ajustando ancho de celdas de los reportes

--- assets/components/php/salidaProductos/reporteSalida.php
+++ assets/components/php/salidaProductos/reporteSalida.php
@@ -2,7 +2,7 @@
 require('../../pdf/fpdf.php');
 
 date_default_timezone_set('America/Mexico_City');
-$fechaHoy = date("d-m-Y");
+$fechaHoy = date("d/m/Y");
 
 require_once "../conexion.php";
     $conexion = conexion();
@@ -15,10 +15,11 @@ class PDF extends FPDF
     //** ENCABEZADO **
 public function Header()
 {
-    $this->Image('ccStore_Azul.jpg',15,10,45); //logo, posicion y tamaño
+    $this->Image('ccStore_Azul.jpg',20,15,45); //logo, posicion y tamaño
     $this->SetFont('Arial','B',10);
     $this->SetTextColor(88, 88, 88);
     $this->SetX(-60);
+    $this->Ln(5);
     //$this->Write(10,'ccStore');//texto y su posicion en Y  
       
 }
@@ -35,11 +36,11 @@ public function Footer()
     $pdf = new PDF();
     $pdf->AliasNbPages();//hace el conteo de las pag
     $pdf->AddPage();//agrega pagina
-    $pdf->SetX(-60);
+    $pdf->SetX(-58);
     $pdf->SetTextColor(88, 88, 88);
-    $pdf->Cell(20,20,'Fecha de creacion: '.$fechaHoy, 0,0,'C',0);
-    $pdf->SetX(-62);
-    $pdf->Cell(20,30,'Pereodo: '.$desde.' / '.$hasta, 0,1,'C',0);
+    $pdf->Cell(20,10,'Fecha de creacion: '.$fechaHoy, 0,0,'C',0);
+    $pdf->SetX(-60);
+    $pdf->Cell(20,20,'Pereodo: '.$desde.' / '.$hasta, 0,1,'C',0);
     //$pdf->Cell(20,20,'Reporte del: '.$desde, 0,1,'C',0);
     $pdf->SetMargins(10,30,20,20); //margen al contenido
     
@@ -48,21 +49,21 @@ public function Footer()
     $pdf->SetFont('Arial','B',13);
     $pdf->SetY(30);//posicion en Y
     $pdf->Ln(10);
-    $pdf->Cell(0,5,'Reporte Salida de Productos', 0,0,'C');
+    $pdf->Cell(0,5,'Reporte salida de productos', 0,0,'C');
     $pdf->Ln(10);//salto de linea y su tamaño
 
     //** Encabezado de la tabla **
     $pdf->SetFont('Arial','B',11);
-    $pdf->SetX(40);//posicion en X
-    $pdf->Cell(40,10,'Codigo de Barras', 0,0,'C',0);
-    $pdf->Cell(31,10,'Marca', 0,0,'C',0);
-    $pdf->Cell(40,10,'Modelo', 0,0,'C',0);
-    $pdf->Cell(25,10,'Cantidad', 0,0,'C',0);
-    $pdf->Cell(41,10,'Fecha', 0,0,'C',0);
-    $pdf->Cell(32,10,'Usuario', 0,1,'C',0);
+    $pdf->SetX(20);//posicion en X
+    $pdf->Cell(40,9,'Codigo de Barras', 0,0,'C',0);
+    $pdf->Cell(60,9,'Marca', 0,0,'C',0);
+    $pdf->Cell(65,9,'Modelo', 0,0,'C',0);
+    $pdf->Cell(20,9,'Cantidad', 0,0,'C',0);
+    $pdf->Cell(41,9,'Fecha', 0,0,'C',0);
+    $pdf->Cell(30,9,'Usuario', 0,1,'C',0);
     $pdf->SetDrawColor(2, 119, 189);//pinta lo que se quiere (linea)
     $pdf->SetLineWidth(1);//grosor de la linea
-    $pdf->Line(41,50,248,50);//linea y posicion
+    $pdf->Line(20,50,275,50);//linea y posicion
 
     //****TABLA SALIDA***** 
     $pdf->Ln(2);//salto de linea tamaño
@@ -81,15 +82,15 @@ public function Footer()
     $query = mysqli_query($conexion,$sentencia);
     
     while($row = $query -> fetch_assoc()){
-        $pdf->SetX(40);//posicion en X
+        $pdf->SetX(20);//posicion en X
         $pdf->SetFillColor(248, 249, 249 ); //relleno de la tabla y su color
 
-        $pdf->Cell(40,10, $row['codigo_barras'], 1,0,'C',1);
-        $pdf->Cell(31,10, $row['marca'], 1,0,'C',1);
-        $pdf->Cell(40,10, $row['modelo'], 1,0,'C',1);
-        $pdf->Cell(25,10, $row['stock'], 1,0,'C',1);
-        $pdf->Cell(41,10, $row['fechaRegistro'], 1,0,'C',1);
-        $pdf->Cell(32,10, $row['usuario'], 1,1,'C',1);
+        $pdf->Cell(40,8, $row['codigo_barras'], 1,0,'C',1);
+        $pdf->Cell(60,8, $row['marca'], 1,0,'C',1);
+        $pdf->Cell(65,8, $row['modelo'], 1,0,'C',1);
+        $pdf->Cell(20,8, $row['stock'], 1,0,'C',1);
+        $pdf->Cell(41,8, $row['fechaRegistro'], 1,0,'C',1);
+        $pdf->Cell(30,8, $row['usuario'], 1,1,'C',1);
     }
     
     $pdf->Output();
